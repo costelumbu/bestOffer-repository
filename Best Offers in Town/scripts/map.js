3a30fbea93babb04eb82c1adfb353db05f1e8d81
Denis Stoyanov <denis.a.stoyanov@gmail.com>
2014-10-18 00:04:30 +0000

    Changes made by Denis Stoyanov (denis.a.stoyanov@gmail.com).

--- Best Offers in Town/scripts/map.js
+++ Best Offers in Town/scripts/map.js
@@ -15,7 +15,6 @@ var Map = (function () {
             map = new google.maps.Map(element, options);
             
             if(navigator.geolocation) {
-                geoSupport = true;
                 navigator.geolocation.getCurrentPosition(function(position) {
                     initialLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
                     map.setCenter(initialLocation);
@@ -27,18 +26,28 @@ var Map = (function () {
                 alert("Your deviced doesn't support Geolocation");
                 initialLocation = new google.maps.LatLng(42.6975100, 23.3241500);
                 map.setCenter(initialLocation);
+
             }
-            /*Offers.stores.fetch(function() {
+        }
+        
+        var showShops = function () {
+            Offers.stores.fetch(function() {
                 var data = this.data();
+                console.log(data.length);
                 for (var i = 0; i < data.length; i++) {
-                    console.log(data[i].Street);
+                    console.log(data[i].Geo.latitude);
+                    var marker = new google.maps.Marker({
+                          position: new google.maps.LatLng(data[i].Geo.latitude, data[i].Geo.longitude),
+                          map: map
+                      });
                 }              
-            });*/
+            });
         }
         
         return { 
-            initMap: initMap
-        }
+            initMap: initMap,
+            showShops: showShops
+        };
     }());
     
     return mapObject;
