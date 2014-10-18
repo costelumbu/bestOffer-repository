818bc5ec0843e99b2546c339636fdf658980020a
Denis Stoyanov <denis.a.stoyanov@gmail.com>
2014-10-18 07:22:42 +0000

    Changes made by Denis Stoyanov (denis.a.stoyanov@gmail.com).

--- Best Offers in Town/scripts/map.js
+++ Best Offers in Town/scripts/map.js
@@ -5,12 +5,13 @@ var Map = (function () {
         var map;
         var initialLocation;
         var current;
+        var infowindow;
 
         var initMap = function () {
             var element = document.getElementById('map_canvas');
             var options = {
               mapTypeId: google.maps.MapTypeId.ROADMAP,
-              zoom: 8
+              zoom: 12
             };
             
             map = new google.maps.Map(element, options);
@@ -21,7 +22,9 @@ var Map = (function () {
                     map.setCenter(initialLocation);
                 }, function () {
                     alert("Geolocation service has failed");
-                }, {timeout:10000});
+                    initialLocation = new google.maps.LatLng(42.6975100, 23.3241500);
+                    map.setCenter(initialLocation);
+                });
             } 
             else {
                 alert("Your deviced doesn't support Geolocation");
@@ -33,21 +36,48 @@ var Map = (function () {
         
         var showShops = function () {
             Offers.stores.fetch(function() {
+                infowindow = new google.maps.InfoWindow({
+                    content: "holding..."
+                });
                 var data = this.data();
-                console.log(data.length);
+                var contentString = '<div id="content">'+
+                  '<div id="siteNotice">'+
+                  '</div>'+
+                  '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
+                  '<div id="bodyContent">'+
+                  '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+                  'sandstone rock formation in the southern part of the '+
+                  'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
+                  'south west of the nearest large town, Alice Springs; 450&#160;km '+
+                  '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
+                  'features of the Uluru - Kata Tjuta National Park. Uluru is '+
+                  'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
+                  'Aboriginal people of the area. It has many springs, waterholes, '+
+                  'rock caves and ancient paintings. Uluru is listed as a World '+
+                  'Heritage Site.</p>'+
+                  '<p>Attribution: Uluru, <a href="http://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
+                  'http://en.wikipedia.org/w/index.php?title=Uluru</a> '+
+                  '(last visited June 22, 2009).</p>'+
+                  '</div>'+
+                  '</div>';
                 for (var i = 0; i < data.length; i++) {
-                    //console.log(data[i].Geo.latitude);
                     var marker = new google.maps.Marker({
-                          position: new google.maps.LatLng(data[i].Geo.latitude, data[i].Geo.longitude),
-                          map: map
+                        position: new google.maps.LatLng(data[i].Geo.latitude, data[i].Geo.longitude),
+                        map: map,
+                        animation: google.maps.Animation.DROP,
+                        html: data[i].Name + "<br/>" + contentString
                       });
+                    google.maps.event.addListener(marker, "click", function () {
+                        map.panTo(this.getPosition());
+                        console.log(this.getPosition());
+                        infowindow.setContent(this.html);
+                        infowindow.open(map, this);
+                    });
+                    
                 }              
             });
         }
-        
-        var parseAddress = function() {
-            
-        }
+
         
         return { 
             initMap: initMap,
